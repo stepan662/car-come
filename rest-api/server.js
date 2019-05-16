288b86b4fa8ec87d0ae2ec8d635ecc2f29fe4604
Radim Podola <rpodola@gmail.com>
2019-05-16 13:05:01 +0200

    Transport Request REST

--- rest-api/server.js
+++ rest-api/server.js
@@ -10,6 +10,11 @@ app.all('/test', function (req, res) {
   res.status(404).send()
 })
 
+app.all('/route', function (req, res) {
+  console.log(req.body)
+  res.json({"carID": 0, "distance": 20000})
+})
+
 app.listen(3000 , err => {
   if (err) throw err
   console.log('> Ready on port 3000')
