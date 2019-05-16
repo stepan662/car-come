28a4062a4342bc1167dd432ec822b9dbe12b7f16
wbadmin <>
2019-05-16 17:08:09 +0200

    {/src/main/java/com/myspace/car_come/InsuranceEventCarOwner.java}

--- src/main/java/com/myspace/car_come/InsuranceEventCarOwner.java
+++ src/main/java/com/myspace/car_come/InsuranceEventCarOwner.java
@@ -9,7 +9,53 @@ public class InsuranceEventCarOwner implements java.io.Serializable {
 
 	static final long serialVersionUID = 1L;
 
+	private java.lang.Integer id;
+	private java.lang.String firstName;
+	private java.lang.String lastName;
+	private java.lang.String email;
+
 	public InsuranceEventCarOwner() {
+	}
+
+	public java.lang.Integer getId() {
+		return this.id;
+	}
+
+	public void setId(java.lang.Integer id) {
+		this.id = id;
+	}
+
+	public java.lang.String getFirstName() {
+		return this.firstName;
+	}
+
+	public void setFirstName(java.lang.String firstName) {
+		this.firstName = firstName;
+	}
+
+	public java.lang.String getLastName() {
+		return this.lastName;
+	}
+
+	public void setLastName(java.lang.String lastName) {
+		this.lastName = lastName;
+	}
+
+	public java.lang.String getEmail() {
+		return this.email;
+	}
+
+	public void setEmail(java.lang.String email) {
+		this.email = email;
+	}
+
+	public InsuranceEventCarOwner(java.lang.Integer id,
+			java.lang.String firstName, java.lang.String lastName,
+			java.lang.String email) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
 	}
 
 }
