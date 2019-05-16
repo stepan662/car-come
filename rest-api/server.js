6e1ae2f74d7bebc6985ab0f58808ebbe5bfe06d7
stepan <granat.stepan@gmail.com>
2019-05-16 18:04:40 +0200

    First draft of "DB"

--- rest-api/server.js
+++ rest-api/server.js
@@ -1,13 +1,36 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
+const testData = require('./testData')
 
 app.use(bodyParser.json())
 
-app.all('/test', function (req, res) {
-  console.log(req.body)
-  // res.json({ isOk: true });
-  res.status(404).send()
+app.all('/get-car-owner', function (req, res) {
+  console.log(req.body, req.query)
+  let owner = null
+  try {
+    const { firstName, lastName } = req.body
+    const user = testData.users.find(u => u.firstName === firstName && u.lastName === lastName)
+    const car = testData.cars.find(c => c.id === user.borrowedCar)
+    owner = testData.users.find(u => u.id === car.ownerId)
+  } catch (e) {
+    console.error(e)
+  }
+
+  if (owner) {
+    console.log("Owner found")
+    const { id, firstName, lastName, email } = owner
+    res.json({
+      id,
+      firstName,
+      lastName,
+      email
+    })
+  } else {
+    console.log("Owner not found")
+    res.status(404).send()
+  }
+
 })
 
 app.all('/route', function (req, res) {
---
+++ rest-api/testData.js
@@ -0,0 +1,20 @@
+module.exports = {
+  users: [{
+    id: 0,
+    firstName: "Stepan",
+    lastName: "Granat",
+    email: "granat.stepan@gmail.com",
+    borrowedCar: 0
+  },{
+    id: 1,
+    firstName: "Hanz",
+    lastName: "Gruber",
+    email: "granat.stepan@gmail.com",
+    borrowedCar: null
+  }],
+  cars: [{
+    id: 0,
+    licence: "IMS0RRY",
+    ownerId: 1
+  }]
+}
