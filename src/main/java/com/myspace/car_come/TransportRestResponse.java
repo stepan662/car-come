b6c46093a517e78887185e136110e7158cb56ed6
wbadmin <>
2019-05-18 18:19:47 +0200

    {/src/main/java/com/myspace/car_come/TransportRestResponse.java}

--- src/main/java/com/myspace/car_come/TransportRestResponse.java
+++ src/main/java/com/myspace/car_come/TransportRestResponse.java
@@ -9,10 +9,16 @@ public class TransportRestResponse implements java.io.Serializable {
 
 	static final long serialVersionUID = 1L;
 
-	@org.kie.api.definition.type.Label(value = "ID of found car")
+	@org.kie.api.definition.type.Label("ID of found car")
 	private java.lang.Integer carID;
-	@org.kie.api.definition.type.Label(value = "Transport distance")
+	@org.kie.api.definition.type.Label("Transport distance")
 	private java.lang.Integer distance;
+
+	@org.kie.api.definition.type.Label(value = "Car model")
+	private java.lang.String carModel;
+
+	@org.kie.api.definition.type.Label(value = "Price rate")
+	private java.lang.Integer rate;
 
 	public TransportRestResponse() {
 	}
@@ -33,10 +39,29 @@ public class TransportRestResponse implements java.io.Serializable {
 		this.distance = distance;
 	}
 
+	public java.lang.String getCarModel() {
+		return this.carModel;
+	}
+
+	public void setCarModel(java.lang.String carModel) {
+		this.carModel = carModel;
+	}
+
+	public java.lang.Integer getRate() {
+		return this.rate;
+	}
+
+	public void setRate(java.lang.Integer rate) {
+		this.rate = rate;
+	}
+
 	public TransportRestResponse(java.lang.Integer carID,
-			java.lang.Integer distance) {
+			java.lang.Integer distance, java.lang.String carModel,
+			java.lang.Integer rate) {
 		this.carID = carID;
 		this.distance = distance;
+		this.carModel = carModel;
+		this.rate = rate;
 	}
 
 }
