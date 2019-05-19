711157d52b89a95aa70ecfb979b73ba2cbe2f1dd
wbadmin <>
2019-05-19 18:13:30 +0200

    {/src/main/java/com/myspace/car_come/TransportRate.java}

--- src/main/java/com/myspace/car_come/TransportRate.java
+++ src/main/java/com/myspace/car_come/TransportRate.java
@@ -6,12 +6,35 @@ package com.myspace.car_come;
 
 public class TransportRate implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public TransportRate() {
-    }
+	@org.kie.api.definition.type.Label(value = "Rating")
+	private java.lang.Integer rating;
+	@org.kie.api.definition.type.Label(value = "Feedback")
+	private java.lang.String comment;
 
+	public TransportRate() {
+	}
 
+	public java.lang.Integer getRating() {
+		return this.rating;
+	}
 
+	public void setRating(java.lang.Integer rating) {
+		this.rating = rating;
+	}
+
+	public java.lang.String getComment() {
+		return this.comment;
+	}
+
+	public void setComment(java.lang.String comment) {
+		this.comment = comment;
+	}
+
+	public TransportRate(java.lang.Integer rating, java.lang.String comment) {
+		this.rating = rating;
+		this.comment = comment;
+	}
 
 }
