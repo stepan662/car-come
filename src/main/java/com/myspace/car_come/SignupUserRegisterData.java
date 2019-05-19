3462188a0e8a69285eb9df7a94de87d498af5ef5
wbadmin <>
2019-05-19 19:02:21 +0200

    {/src/main/java/com/myspace/car_come/SignupUserRegisterData.java}

--- src/main/java/com/myspace/car_come/SignupUserRegisterData.java
+++ src/main/java/com/myspace/car_come/SignupUserRegisterData.java
@@ -6,12 +6,47 @@ package com.myspace.car_come;
 
 public class SignupUserRegisterData implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public SignupUserRegisterData() {
-    }
+	@org.kie.api.definition.type.Label(value = "First name")
+	private java.lang.String firstName;
+	@org.kie.api.definition.type.Label(value = "Last name")
+	private java.lang.String lastName;
+	@org.kie.api.definition.type.Label(value = "Email")
+	private java.lang.String email;
 
+	public SignupUserRegisterData() {
+	}
 
+	public java.lang.String getFirstName() {
+		return this.firstName;
+	}
 
+	public void setFirstName(java.lang.String firstName) {
+		this.firstName = firstName;
+	}
+
+	public java.lang.String getLastName() {
+		return this.lastName;
+	}
+
+	public void setLastName(java.lang.String lastName) {
+		this.lastName = lastName;
+	}
+
+	public java.lang.String getEmail() {
+		return this.email;
+	}
+
+	public void setEmail(java.lang.String email) {
+		this.email = email;
+	}
+
+	public SignupUserRegisterData(java.lang.String firstName,
+			java.lang.String lastName, java.lang.String email) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
 
 }
